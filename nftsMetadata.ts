4f8172d293e9f75c98c12e492f949ae8ec705fae
Matheus Pagani <matheus@ventureminer.com>
2024-04-13 21:00:40 -0300

    Uploading nftsMetadata.ts

---
+++ nftsMetadata.ts
@@ -0,0 +1,91 @@
+const nftsMetadata = [
+  {
+    name: "Ethereum Frogs  #1",
+    description:
+      "Efrogs is the OG PFP collection on Linea, the L2 made by Ethereum co-founder Joe Lubin—2015 frogs for the same year of Ethereum's launch.",
+    attributes: [
+      { trait_type: "Background", value: "Rainbow" },
+      { trait_type: "Body Base", value: "Spotted" },
+      { trait_type: "Outerwear", value: "None" },
+      { trait_type: "Headwear", value: "Pirate Hat" },
+      { trait_type: "Eyes", value: "Red" },
+      { trait_type: "Mouth", value: "Smirk" },
+      { trait_type: "Accessory", value: "None" },
+      { trait_type: "Trait Count", value: "7" },
+    ],
+    image: "https://i.nfte.ai/ia/m901/49940/2201984898386784510_999678986.avif",
+    external_url: "https://linktr.ee/efrogs",
+  },
+  {
+    name: "Ethereum Frogs  #2",
+    description:
+      "Efrogs is the OG PFP collection on Linea, the L2 made by Ethereum co-founder Joe Lubin—2015 frogs for the same year of Ethereum's launch.",
+    attributes: [
+      { trait_type: "Background", value: "Grey" },
+      { trait_type: "Body Base", value: "Blue" },
+      { trait_type: "Outerwear", value: "None" },
+      { trait_type: "Headwear", value: "Blue Party Hat" },
+      { trait_type: "Eyes", value: "Angry" },
+      { trait_type: "Mouth", value: "Toothy Smile" },
+      { trait_type: "Accessory", value: "None" },
+      { trait_type: "Trait Count", value: "7" },
+    ],
+    image: "https://i.nfte.ai/ia/m901/49940/2201984898386665726_1964577424.avif",
+    external_url: "https://linktr.ee/efrogs",
+  },
+  {
+    name: "Ethereum Frogs  #3",
+    description:
+      "Efrogs is the OG PFP collection on Linea, the L2 made by Ethereum co-founder Joe Lubin—2015 frogs for the same year of Ethereum's launch.",
+    attributes: [
+      { trait_type: "Background", value: "Orange" },
+      { trait_type: "Body Base", value: "Furry" },
+      { trait_type: "Outerwear", value: "None" },
+      { trait_type: "Headwear", value: "None" },
+      { trait_type: "Eyes", value: "Meme Sunglasses" },
+      { trait_type: "Mouth", value: "Cat Smiley" },
+      { trait_type: "Accessory", value: "None" },
+      { trait_type: "Trait Count", value: "7" },
+    ],
+    image: "https://i.nfte.ai/ia/m901/49940/2201984898386694398_1969851635.avif",
+    external_url: "https://linktr.ee/efrogs",
+  },
+  {
+    name: "Ethereum Frogs  #4",
+    description:
+      "Efrogs is the OG PFP collection on Linea, the L2 made by Ethereum co-founder Joe Lubin—2015 frogs for the same year of Ethereum's launch.",
+    attributes: [
+      { trait_type: "Background", value: "Purple" },
+      { trait_type: "Body Base", value: "Green" },
+      { trait_type: "Outerwear", value: "Puffer Coat" },
+      { trait_type: "Headwear", value: "Moon" },
+      { trait_type: "Eyes", value: "Light Green" },
+      { trait_type: "Mouth", value: "Happy" },
+      { trait_type: "Accessory", value: "None" },
+      { trait_type: "Trait Count", value: "7" },
+    ],
+    image: "https://i.nfte.ai/ia/m901/49940/2201984898386428158_558520879.avif",
+    external_url: "https://linktr.ee/efrogs",
+  },
+  {
+    name: "Ethereum Frogs  #5",
+    description:
+      "Efrogs is the OG PFP collection on Linea, the L2 made by Ethereum co-founder Joe Lubin—2015 frogs for the same year of Ethereum's launch.",
+    attributes: [
+      { trait_type: "Background", value: "Linea Green" },
+      { trait_type: "Body Base", value: "Red" },
+      { trait_type: "Outerwear", value: "None" },
+      { trait_type: "Headwear", value: "None" },
+      { trait_type: "Eyes", value: "Blue" },
+      { trait_type: "Mouth", value: "Cat Smiley" },
+      { trait_type: "Accessory", value: "None" },
+      { trait_type: "Trait Count", value: "7" },
+    ],
+    image: "https://i.nfte.ai/ia/m901/49940/2201984898386407678_290879490.avif",
+    external_url: "https://linktr.ee/efrogs",
+  },
+];
+
+export type NFTMetaData = (typeof nftsMetadata)[number];
+
+export default nftsMetadata;
